b6c8566df7869eb12c7c5653beccd321c1519cf9
Durotoluwa Praise <praiseoluwabumi@@gmail.com>
2026-04-03 06:35:51 +0100

    Fix Netlify build config

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -3,16 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ["127.0.0.1", "localhost"],
-  turbopack: {
-    root: "C:/Users/Praisecrack/Documents/competitive-coding-platform/frontend-nextjs",
-  },
-  // Proxy API requests to Go backend
+
   async rewrites() {
-    const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8080";
+    const backendURL =
+      process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8080";
+
     return [
       {
-        source: '/api/:path*',       // frontend calls /api/challenges
-        destination: `${backendURL}/:path*`, // Go backend
+        source: "/api/:path*",
+        destination: `${backendURL}/:path*`,
       },
     ];
   },
