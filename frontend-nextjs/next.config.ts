5e740aad4b7a5df253f947a526d09d0b63f1b64c
Durotoluwa Praise <praiseoluwabumi@@gmail.com>
2026-04-04 21:54:15 +0100

    updated next config

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   allowedDevOrigins: ["127.0.0.1", "localhost"],
@@ -8,7 +10,10 @@ const nextConfig: NextConfig = {
 
   async rewrites() {
     const backendURL =
-      process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8080";
+      process.env.NEXT_PUBLIC_BACKEND_URL ||
+      (isProduction
+        ? "https://codemaster-q9oo.onrender.com"
+        : "http://127.0.0.1:8080");
 
     return [
       {
